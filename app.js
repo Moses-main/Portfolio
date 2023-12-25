9e84892dcd9b675facf607d8266995a6c3c31065
Moses <Moseschizaram8@gmail.com>
2023-12-25 06:57:19 +0100

    Changed the project to a node application

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = 3500;
+
+const portfolioCont = require("./controllers/portfolio");
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/portfolio", portfolioCont);
+
+app / app.listen(PORT, () => console.log(`app listening on ${PORT}`));
