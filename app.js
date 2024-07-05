d9e2880533b1982545724c686034b1aa4961a2ac
Moses <Moseschizaram8@gmail.com>
2024-07-05 03:52:49 +0100

    added an about page but needs to style it and arrange it properly

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 app.get("/portfolio", portfolioCont);
 
 app / app.listen(PORT, () => console.log(`app listening on ${PORT}`));
